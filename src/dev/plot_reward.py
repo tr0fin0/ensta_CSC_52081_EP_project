45391042efad230835437400c368b4a6dcddd461
Rafael Fernandes <rafaelbenzi@estudante.ufscar.br>
2025-03-17 11:31:03 +0100

    code corrections and refining

--- src/dev/plot_reward.py
+++ src/dev/plot_reward.py
@@ -8,7 +8,7 @@ if is_ipython:
     from IPython import display
 
 
-def plot_reward(episode_num, reward_list, actions) -> None:
+def plot_reward(episode_num, reward_list, actions, save_dir) -> None:
     """
     Plots the reward per episode and the moving average of the reward.
 
@@ -21,10 +21,10 @@ def plot_reward(episode_num, reward_list, actions) -> None:
         None
     """
     plt.figure(1)
-    rewards_tensor = torch.tensor(reward_list, dtype=torch.float)
+    rewards_tensor = torch.tensor(reward_list, dtype=torch.float).to("cuda")
 
     if len(rewards_tensor) >= 11:
-        eval_reward = torch.clone(rewards_tensor[-10:])
+        eval_reward = torch.clone(rewards_tensor[-10:]).to("cuda")
         mean_eval_reward = round(torch.mean(eval_reward).item(), 2)
         std_eval_reward = round(torch.std(eval_reward).item(), 2)
 
@@ -41,12 +41,14 @@ def plot_reward(episode_num, reward_list, actions) -> None:
     plt.plot(rewards_tensor.numpy())
 
     if len(rewards_tensor) >= 50:
-        reward_f = torch.clone(rewards_tensor[:50])
+        reward_f = torch.clone(rewards_tensor[:50]).to("cuda")
         means = rewards_tensor.unfold(0, 50, 1).mean(1).view(-1)
         means = torch.cat((torch.ones(49)*torch.mean(reward_f), means))
         plt.plot(means.numpy())
+    
+    plt.savefig(f"{save_dir}/reward_plot.png")
 
-    plt.pause(0.001)
+    #plt.pause(0.001)
     if is_ipython:
         display.display(plt.gcf())
         display.clear_output(wait=True)
